493c7b30b367aaf73f48188dec05fdcf4c49a353
Prakash Raut <prakash_raut@outlook.com>
2025-07-29 16:03:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+	 // Write your code here
+  const char_map = new Map();
+
+  for (let ch of str) {
+    if (char_map.has(ch)) {
+      char_map.set(ch, char_map.get(ch) + 1);
+    } else {  
+      char_map.set(ch, 1);
+    }
+  }
+
+  for (let ch of str) {
+    if (char_map.get(ch) === 1) {
+      return ch;
+    }
+  }
+
+  return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
